9fa8baf7f9b2fa200b4e49bb67419dbb8c6a8ef5
EvridikiLam <evridiki.lam@gmail.com>
2026-06-27 12:25:12 +0300

    Favorite Card component

---
+++ src/components/FavoriteCard.jsx
@@ -0,0 +1,20 @@
+const FavoriteCard = ({data, unit, onRemove}) => {
+    const temperature =
+        unit === "metric" ? data.main.temp : (data.main.temp * 9) / 5 + 32;
+    const unitSymbol = unit === "metric" ? "°C" : "°F"; 
+    
+    return (
+        <div>
+            <h3>{data.name}, {data.sys.country}</h3>
+            <img 
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`} 
+                alt={data.weather[0].description} 
+                />
+                <p>{Math.round(temperature)} {unitSymbol}</p>
+                <p>{data.weather[0].description}</p>
+                <button onClick={() => onRemove(data.name)}>Remove</button>
+        </div>
+    );
+};
+
+export default FavoriteCard;
