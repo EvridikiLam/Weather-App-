9c9c1f444bd6f91ae2b791115896354409f6c56a
EvridikiLam <evridiki.lam@gmail.com>
2026-06-29 18:42:21 +0300

    some changes to style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="page">
       <h1>404 -_- </h1>
       <p>Oops! This page doesn't exist.</p>
       <button onClick={() => navigate("/")}>Go back Home</button>
